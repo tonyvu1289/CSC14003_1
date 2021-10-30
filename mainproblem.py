81750c40b55d48791075db9edf1dfb0228034399
Gia Thuận <togiathuan@gmail.com>
2021-10-30 17:14:59 +0700

    big-update

---
+++ mainproblem.py
@@ -0,0 +1,130 @@
+from typing import List, Any
+import math
+import problem_solution
+import ulity
+import os
+import cv2 as cv
+import re
+import numpy as np
+import copy
+np.seterr(over='ignore') # ignore RuntimeWarning: overflow encountered in ubyte_scalars
+
+
+class Map:
+    def set(self, pos, value):
+        self.data[pos[0], pos[1]] = value
+
+    def all_move(self, pos):
+        # all move can perform from the position pos
+        # return action and new position
+        ans = []
+        dx = [0,0,1,-1]
+        dy = [1,-1,0,0]
+        for i in range(4):
+            newPos: tuple[int, int] = (pos[0] + dx[i], pos[1] + dy[i])
+            #4 directions
+            if (not pos == newPos) and ( newPos[0] >= 0) and (newPos[1] >= 0):
+                if dx[i] == 0 and dy[i] == 1:
+                    ans.append(('right', newPos))
+                if dx[i] == 0 and dy[i] == -1:
+                    ans.append(('left', newPos))
+                if dx[i] == 1 and dy[i] == 0:
+                    ans.append(('down', newPos))
+                if dx[i] == -1 and dy[i] == 0:
+                    ans.append(('up', newPos))
+        return ans
+
+class PathWaySearchProblem(problem_solution.SearchProblem):
+    def __init__(self, input_pathway):
+        self.count_show = 0
+        # input is a array contain all data number
+        self.bonus_points, self.matrix = ulity.read_file(input_pathway)
+        self.map = Map()
+        # assign data to init state and goal state
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[0])):
+                if self.matrix[i][j] == 'S':
+                    self.init = (i,j)
+                elif self.matrix[i][j]==' ':
+                    if (i==0) or (i==len(self.matrix)-1) or (j==0) or (j==len(self.matrix[0])-1):
+                        self.goal = (i,j)
+                else:
+                    pass
+    #check is at init state
+    def initState(self):
+        return self.init
+
+    #check is at goal state
+    def isGoal(self, state):
+        return self.goal == state
+    
+    def heuristic(self,state:tuple[int,int]):
+        #Distance Manhattan |x1-x2|+|y1-y2|
+        h = (math.fabs(state[0] - self.goal[0])) + (math.fabs(state[1]-self.goal[1]))
+        return h
+       
+    def expandSuccessor(self, state):
+        ans = []
+        for action, newstate in self.map.all_move(state):
+            if self.matrix[newstate[0]][newstate[1]] == 'x':
+                continue
+            #Cost to move 1 step is 1
+            addcost = 1
+            ans.append((action,newstate,addcost))
+        return ans
+
+
+
+#DEBUG SESSION : test class PathWaySearchProblem
+#Aglo Astar
+# 5 maps without bouns points
+for i in range(1,6):
+    map_name = 'maze_map'
+    map_id = str(i)
+    map_extesion = '.txt'
+
+    #print id map
+    print(' '.join(['Maze Map',map_id]))
+
+    #solve problem
+    problem = PathWaySearchProblem(str(map_name+map_id+map_extesion))
+    sovler1 = problem_solution.A_StarSolution()
+    sovler1.solve(problem)
+    list_actions = sovler1.actions
+    routes = []
+
+    #init State
+    routes.append(problem.init)
+
+    #route (go to goal)
+    i = 0
+    for route in list_actions:
+        route: tuple[int,int] = list_actions[i][1]
+        routes.append(route)
+        i += 1
+    
+    #Visualize map
+    ulity.visualize_maze(problem.matrix,problem.bonus_points,problem.init,problem.goal,routes)
+
+
+# # #heuristic 2
+
+
+# #heuristic 3
+# print("#heuristic 3 :")
+# sovler2.setHeuristic(3)
+# sovler2.solve(problem,1)
+# newmap3 = Map("map.bmp")
+# for action, reached_state in sovler2.actions:
+#     newmap3.set(reached_state,255)
+# cv.imwrite("map3.bmp", newmap3.data)
+# problem.cleanScreen()
+# #UCS
+# print("#UCS algo :")
+# sovler3_UCS = problem_solution.UniformSolution()
+# sovler3_UCS.solve(problem, 1)
+# newmap3= Map("map.bmp")
+# for action, reached_state in sovler3_UCS.actions:
+#     newmap3.set(reached_state,255)
+# cv.imwrite("map3_optimal.bmp", newmap3.data)
+# #END SESION
---
+++ problem_solution.py
@@ -0,0 +1,153 @@
+import heapq
+
+
+class PriorityQueue:
+    def __init__(self) -> None:
+        # priority is a "element to priority value of that element" dictionary
+        self.heap = []
+        self.priority = {}
+        self.DONE = -10000000
+
+    def isEmpty(self):
+        return len(self.priority) == 0
+
+    def add_update(self, element, priorityValue):
+        if element in self.priority:  #
+            if self.priority[element] <= priorityValue:
+                return 0  # not success
+        self.priority[element] = priorityValue
+        heapq.heappush(self.heap, (priorityValue, element))
+        return 1  # success
+
+    def pop_delete(self):
+        while len(self.heap) > 0:
+            priority, state = heapq.heappop(self.heap)
+            if self.priority[state] == self.DONE: continue  # Outdated priority, skip
+            self.priority[state] = self.DONE
+            return state, priority
+        return (None, None)  # Nothing left...
+
+
+class SearchProblem:
+    """class trừu tượng. Những hàm trong class này là hàm ảo"""
+
+    def __init__(self):
+        raise NotImplementedError("No __init__ yet!")
+
+    def initState(self): raise NotImplementedError("No initState() implementation yet!")
+
+    def isGoal(self, state): raise NotImplementedError("No isGoal() implementation yet!")
+
+    def expandSuccessor(self, state): raise NotImplementedError("No expandSuccessor implementation yet!")
+
+    def display_explored(self, state): pass
+
+
+class Solution:
+    """class trừu tượng cho những chiến lược tìm kiếm"""
+
+    def __init__(self):
+        raise NotImplementedError("No __init__() implementation yet!")
+
+    def solve(self, problem : SearchProblem, display=0):
+        raise NotImplementedError("No solve() implementation yet!")
+
+
+class A_StarSolution(Solution):
+    def __init__(self):
+        self.heuristic_choice = 1
+
+    def setHeuristic(self, choice):
+        self.heuristic_choice = choice
+
+    def solve(self, problem, display=0):
+        self.totalCost = 0
+        self.totalExpand = 0
+        self.actions = []
+        # this list is used to display result which is a sequence of actions and the state after the action
+        self.cost = {}
+        # a dictionary Map a state to how much cost to reach that state
+        self.backPointer = {}
+        # a dictionary Map a state to its (action,parent state)
+        frontier = PriorityQueue()
+        # in A_star, i will use priority queue to choose which state have a min g(n)+h(n)
+        # with g(n) is a certain cost to reach state n, h(n) is a heuristic func
+        # after have solved, frontier is no use. So I will make it wiped out =))))
+        frontier.add_update(problem.initState(), 0)
+        self.cost[problem.initState()] = 0
+        while not frontier.isEmpty():
+            parentState = frontier.pop_delete()[0]
+            parentCost = self.cost[parentState]
+            self.totalExpand = self.totalExpand + 1
+            if problem.isGoal(parentState):
+                state = parentState
+                self.totalCost = self.cost[state]
+                while state is not problem.initState():
+                    act, par = self.backPointer[state]
+                    self.actions.append((act, state))
+                    state = par
+                self.actions.reverse()
+                if display >= 2:
+                    for act, reach_state in self.actions:
+                        print("Do %s to reach state %s \n" % (act, reach_state))
+                if (display >= 1):
+                    print("Total node expand : %s \n "
+                          "Total cost to reach goal : %s" % (self.totalExpand, self.totalCost))
+                return
+            if display >= 2:
+                print(("Expanding %s with reached cost is %s" % (parentState, parentCost)))
+            if display >= 1:
+                problem.display_explored(parentState)
+            for action, newState, addCost in problem.expandSuccessor(parentState):
+                if (newState in self.cost) and (self.cost[newState] <= parentCost + addCost):
+                    # tránh bị vòng lặp
+                    continue
+                if frontier.add_update(newState,
+                                       parentCost + addCost + problem.heuristic(newState)):
+                    self.cost[newState] = parentCost + addCost
+                    self.backPointer[newState] = (action, parentState)
+
+        print("No path found or something happened and my alogoritm failed :(")
+
+
+class UniformSolution(Solution):
+    def __init__(self):
+        pass
+
+    def solve(self, problem, display=0):
+        self.totalCost = 0
+        # total path-cost of solution
+        self.totalExpand = 0
+        # count nodes have been expanded
+        self.actions = []
+        # a sequence of action (final solution)
+        self.backPointer = {}
+        # backPointer is dict state : (action,parent-state)
+        frontier = PriorityQueue()
+        # In UCS, the priority queue is used to define the minium path-cost node (state,path-cost)
+        frontier.add_update(problem.initState(), 0)
+        while not frontier.isEmpty():
+            parentState, parentCost = frontier.pop_delete()
+            self.totalExpand = self.totalExpand + 1
+            if problem.isGoal(parentState):
+                state = parentState
+                self.totalCost = parentCost
+                while state is not problem.initState():
+                    act, par = self.backPointer[state]
+                    self.actions.append((act, state))
+                    state = par
+                self.actions.reverse()
+                if display >= 2:
+                    for act, reach_state in self.actions:
+                        print("Do %s to reach state %s \n" % (act, reach_state))
+                if display >= 1:
+                    print("Total node expand : %s \n "
+                          "Total cost to reach goal : %s" % (self.totalExpand, self.totalCost))
+                return
+            if display >= 2:
+                print(("Expanding %s with reached cost is %s" % (parentState, parentCost)))
+            if display >= 1:
+                problem.display_explored(parentState)
+            for action, newState, addCost in problem.expandSuccessor(parentState):
+                if frontier.add_update(newState, parentCost + addCost):
+                    self.backPointer[newState] = (action, parentState)
